ba44dbed14a6f138ed5e28ecbc7b50e61321e8d5
LeeChangMin <dlckdals1004@naver.com>
2023-02-26 03:11:30 +0900

    pd

--- practice_pd2.py
+++ practice_pd2.py
@@ -42,6 +42,33 @@ DF.loc[0,'call'] = '01034341234'
 # print(DF.notnull())
 
 
-print(DF.dropna())
+# print(DF.dropna())
 DF['call'] = DF['call'].fillna('전화번호 없음')
-print(DF)
+# print(DF)
+
+A = pd.Series([2,4,6], index=[0,1,2])
+B = pd.Series([1,3,5], index=[1,2,3])
+# print(A+B)
+
+# print(A.add(B, fill_value=0))
+
+A = pd.DataFrame(np.random.randint(0,10,(2,2)), columns=list("AB"))
+B = pd.DataFrame(np.random.randint(0,10,(3,3)), columns=list("BAC"))
+# print(A+B)
+
+data = {
+  'A': [i+5 for i in range(3)],
+  'B' : [i**2 for i in range(3)]
+}
+DF = pd.DataFrame(data)
+# print(DF['A'].sum()) # 18
+# print(DF.sum())
+# print(DF.mean())
+
+df = pd.DataFrame({
+  'col1' : [2,1,9,8,7,4],
+  'col2' : ['A','A','B',np.nan, 'D','C'],
+  'col3' : [0,1,9,4,2,3]
+})
+
+print(df.sort_values(['col2','col1']))
